646136b967ecce9282c15506b0f902c22617930b
Nemanja Radosavljevic <shoocky1337@gmail.com>
2016-06-20 23:39:33 +0200

    added ng2-semantic, and accordion example

--- ts/components/NewVerificationCallComponent.ts
+++ ts/components/NewVerificationCallComponent.ts
@@ -1,51 +1,143 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 import {VerificationCallService} from '../services/verification-call.service';
 import {VerificationCall} from '../classes/verification-call';
 import {RouterShareService} from '../services/router-share.service';
 import {RouteParams, RouteRegistry, Router} from '@angular/router-deprecated';
+import {DIRECTIVES} from 'ng2-semantic-ui/ng2-semantic-ui';
+
+declare var jQuery: any;
 
 @Component({
     selector: 'new-verificaion-call',
-    template:`
-        <h1> New verification call </h1>
-        <ul *ngFor="let flag of flags">
-            <li>{{ flag.name}} {{flag.value}}</li>
-        </ul>
-    `
+    directives: [DIRECTIVES],
+    templateUrl: 'templates/new-verification-call.component.html'
 })
 export class NewVerificationCallComponent implements OnInit{
-    flags = [
-        {name : 'check-assert', value: true},
-        {name : 'check-block-conds', value : false},
-        {name : 'check-div-zero', value : true},
-        {name : 'check-pointers', value : true},
-        {name : 'enable-parallel', value : false},
-        {name : 'find-first-flawed', value : false},
-        {name : 'light', value : false},
-        {name : 'memory-flag', value : true},
-        {name : 'model', value : false},
-        {name : 'print-html', value : false},
-        {name : 'skip-inside-loop', value : true},
-        {name : 'solver', value : ''},
-        {name : 'students-mode', value : false},
-        {name : 'track-pointers', value : true},
-        {name : 'track-unrecheable', value : false},
-        {name : 'urm', value : false},
-        {name : 'enable-optimizations', value : false},
-        {name : 'loop-unroll-begin', value : 2},
-        {name : 'loop-unroll-end', value : 1},
-        {name : 'number-threads', value : 4},
-        {name : 'timeout', value : 0},
-        {name : 'starting-function', value : 'main'}
-    ];
 
     constructor(private verificatonCallService: VerificationCallService,
-                private routerShareService: RouterShareService){
+                private routerShareService: RouterShareService,
+                private elementRef:ElementRef){
 
     }
 
     ngOnInit() {
+
         let id = this.routerShareService.getId();
         console.log(this.flags);
+
     }
+
+flags = [
+        {
+            name : 'check-assert',
+            value: null,
+            default: true
+        },
+        {
+            name : 'check-block-conds',
+            value: null,
+            default: false
+        },
+        {
+            name : 'check-div-zero', 
+            value : null,
+            default: true
+        },
+        {
+            name : 'check-pointers',
+            value: null,
+            default : true
+        },
+        {
+            name : 'enable-parallel',
+            value: null,
+            default : false
+        },
+        {
+            name : 'find-first-flawed',
+            value: null,
+            default : false
+        },
+        {
+            name : 'light',
+            value: null,
+            default : false
+        },
+        {
+            name : 'memory-flag',
+            value: null,
+            default : true
+        },
+        {
+            name : 'model',
+            value: null,
+            default : false
+        },
+        {
+            name : 'print-html',
+            value: null,
+            default : false
+        },
+        {
+            name : 'skip-inside-loop',
+            value: null,
+            default : true
+        },
+        {
+            name : 'solver',
+            value: null,
+            default : ''
+        },
+        {
+            name : 'students-mode',
+            value: null,
+            default : false
+        },
+        {
+            name : 'track-pointers',
+            value: null,
+            default : true
+        },
+        {
+            name : 'track-unrecheable',
+            value: null,
+            default : false
+        },
+        {
+            name : 'urm',
+            value: null,
+            default : false
+        },
+        {
+            name : 'enable-optimizations',
+            value: null,
+            default : false
+        },
+        {
+            name : 'loop-unroll-begin',
+            value: null,
+            default : 2
+        },
+        {
+            name : 'loop-unroll-end',
+            value: null,
+            default : 1
+        },
+        {
+            name : 'number-threads',
+            value: null,
+            default : 4
+        },
+        {
+            name : 'timeout',
+            value: null,
+            default : 0
+        },
+        {
+            name : 'starting-function',
+            value: null,
+            default : 'main'
+        }
+    ];
+
 }
